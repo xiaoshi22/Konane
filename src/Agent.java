b925245f53669daaa7e8a044578f3b4124e9b133
Xiaoshi Wang <xiaoshiwang22@gmail.com>
2018-10-22 14:45:24 -0400

    commit for pull

--- src/Agent.java
+++ src/Agent.java
@@ -23,6 +23,23 @@ public class Agent {
         return ret;
     }
 
+
+    public Board alphaBetaSearch(Board board) {
+        List<Board> successors = board.getSuccessors();
+        int utility = Integer.MIN_VALUE;
+        Board ret = null;
+        for(Board successor: successors){
+            int tempUtility = alphaBetaValue(successor, 2);
+//            System.out.println("Utility: "+ tempUtility);
+//            System.out.println(successor);
+            if (tempUtility > utility){
+                utility = tempUtility;
+                ret = successor;
+            }
+        }
+        return ret;
+    }
+
     private int minmaxValue(Board board, int curDepth){
         List<Board> successors = board.getSuccessors();
         int utility = successors.size();
@@ -40,6 +57,34 @@ public class Agent {
                 utility = Integer.MAX_VALUE;
                 for (Board successor : successors) {
                     int tempUtility = minmaxValue(successor, curDepth + 1);
+                    if (tempUtility < utility) {
+                        utility = tempUtility;
+                    }
+                }
+            }
+        }
+        return utility;
+    }
+
+
+
+    private int alphaBetaValue(Board board, int curDepth){
+        List<Board> successors = board.getSuccessors();
+        int utility = successors.size();
+        if(curDepth<= limitDepth && utility != 0) {
+            boolean isMax = (curDepth % 2 == 1);
+            if (isMax) {
+                utility = Integer.MIN_VALUE;
+                for (Board successor : successors) {
+                    int tempUtility = alphaBetaValue(successor, curDepth + 1);
+                    if (tempUtility > utility) {
+                        utility = tempUtility;
+                    }
+                }
+            } else {
+                utility = Integer.MAX_VALUE;
+                for (Board successor : successors) {
+                    int tempUtility = alphaBetaValue(successor, curDepth + 1);
                     if (tempUtility < utility) {
                         utility = tempUtility;
                     }
